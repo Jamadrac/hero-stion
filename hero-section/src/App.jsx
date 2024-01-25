79159b9bfa59dd404b34230b5a75b6920435c1a8
jamadrac <rabbikindlo001@gmail.com>
2024-01-25 16:25:36 +0200

    first commit

---
+++ hero-section/src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+   <div className="min-h-screen flex items-center justify-center w-full">
+   <form className="  bg-white p-4 w-full md:max-w-sm">
+    <input type='text'className="p-3 border shadow"/>
+
+   </form>
+   </div>
+  )
+}  
